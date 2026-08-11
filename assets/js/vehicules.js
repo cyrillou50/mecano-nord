b00749bd07ac80fa90653f5bb650f1417b0e12c9
cyrillou50 <72271687+cyrillou50@users.noreply.github.com>
2026-08-11 13:38:01 +0200

    Coffre

--- assets/js/vehicules.js
+++ assets/js/vehicules.js
@@ -249,7 +249,7 @@
           '<div class="vboxes">' +
             boite("Carburant", v.carburant) +
             boite("Places", v.places || "") +
-            boite("Coffre", v.coffre) +
+            boite("Coffre (KG)", v.coffre) +
             boite("Réservoir", v.litres ? v.litres + " L" : "") +
           "</div>" +
         "</div>" +
